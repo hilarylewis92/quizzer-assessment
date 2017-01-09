2f3eae38cb17922411eb3fe2d47b54b45d6cf1e3
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-08 17:22:21 -0700

    Component will mount grabs data from api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
+import Quizzes from './Quizzes'
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      quizzes: [],
+    }
+  }
+
+  componentWillMount() {
+    this.getQuizzes()
+  }
+
+  getQuizzes() {
+    axios.get('/quizzes')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div
+        className="App">
+
+        <h2>
+          What JavaScript Error Are You?
+        </h2>
+
+        <Quizzes />
+
       </div>
     );
   }
---
+++ src/Quizzes.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Quizzes extends Component {
+
+  render() {
+    return (
+      <div
+        className="Quizzes">
+
+
+
+      </div>
+    );
+  }
+}
+
+export default Quizzes;
