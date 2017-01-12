b60f75023871ce29735ec02e3e8827185bc802b0
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-12 12:00:36 -0700

    On submit, user sends selected score to database and recieves their score

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ export default class App extends Component {
     super()
     this.state = {
       quizzes: null,
+      answer: '',
+      selectedAnswer: {},
+      selectedScore: '',
     }
   }
 
@@ -29,15 +32,55 @@ export default class App extends Component {
       })
   }
 
+  getAnswerScores () {
+    axios.post('/scores', {
+      score: this.state.selectedScore
+      })
+      .then((response) => {
+        this.setState ({
+          answer: response.data.score
+        })
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  handleSelectedAnswers (e, score, i) {
+    var currentSelectedAnswers = this.state.selectedAnswer
+
+    currentSelectedAnswers[i] = score
+
+    this.setState ({
+      selectedAnswer: currentSelectedAnswers
+    })
+    this.addSelectedAnswers(i)
+  }
+
+  addSelectedAnswers(i) {
+    const { selectedAnswer } = this.state
+
+    let sum = Object.keys(selectedAnswer).reduce((sum, i) => {
+      return sum + parseInt(selectedAnswer[i])
+    }, 0)
+    this.setState ({
+      selectedScore: sum
+    })
+  }
+
   render() {
-    const { quizzes } = this.state
+    const { quizzes, answer } = this.state
 
     return (
       <div
         className="App">
 
         {quizzes
-        ? <Quiz quizzes={quizzes} />
+        ? <Quiz quizzes={quizzes}
+          answer={answer}
+          getAnswerScores={(e) => this.getAnswerScores(e)}
+          handleSelectedAnswers={(e, score, i) => this.handleSelectedAnswers(e, score, i)}/>
         : null}
 
       </div>
